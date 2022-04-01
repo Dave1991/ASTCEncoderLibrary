23fdf27d96e54ee3ab31c4f4685c678b01a445e3
forrestlin <forrestlin@tencent.com>
2022-04-01 14:44:05 +0800

    Initial Commit

---
+++ ASTCEncoderLibrary/ASTCEncoderLibrary.h
@@ -0,0 +1,12 @@
+//
+//  ASTCEncoderLibrary.h
+//  ASTCEncoderLibrary
+//
+//  Created by forrest on 2022/4/1.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ASTCEncoderLibrary : NSObject
+
+@end
---
+++ ASTCEncoderLibrary/ASTCEncoderLibrary.m
@@ -0,0 +1,12 @@
+//
+//  ASTCEncoderLibrary.m
+//  ASTCEncoderLibrary
+//
+//  Created by forrest on 2022/4/1.
+//
+
+#import "ASTCEncoderLibrary.h"
+
+@implementation ASTCEncoderLibrary
+
+@end
